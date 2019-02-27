b87a16b07afa7ffc923a93573b80f61e537cf073
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-27 11:17:39 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,9 @@
 # Code your solution here!
+def run_guessing_game
+input = ""
+while input
+    puts "Guess a number between 1 and 6."
+    input = gets.downcase.chomp
+    random_number = rand(1..6).to_s
+  end
+end
